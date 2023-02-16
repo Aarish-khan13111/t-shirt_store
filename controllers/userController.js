f72d06837b6fdc5b75991eb9dec99657e0e8635c
aarish <aarishkhan13111@gmail.com>
2023-02-16 13:35:22 +0530

    backend for Tshirt store

---
+++ controllers/userController.js
@@ -0,0 +1,77 @@
+const User = require("../models/user");
+const BigPromise = require("../middlewares/bigPromise");
+const CustomError = require("../utils/customError");
+const cookieToken = require("../utils/cookieToken");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+
+exports.signup = BigPromise(async (req, res, next) => {
+  let result;
+  if (req.files) {
+    let file = req.files.photo;
+    result = await cloudinary.uploader.upload(file.tempFilePath, {
+      folder: "users",
+      width: 150,
+      crop: "scale",
+    });
+  }
+
+  const { name, email, password } = req.body;
+
+  if (!email || !name || !password) {
+    return next(new CustomError("Name, email, password are required", 400));
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    photo: {
+      id: result.public_id,
+      secure_url: result.secure_url,
+    },
+  });
+
+  cookieToken(user, res);
+});
+
+exports.login = BigPromise(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //check for presence of password
+  if (!email || !password) {
+    return next(new CustomError("please provid email and password", 400));
+  }
+
+  //getting user from database
+  const user = await User.findOne({ email }).select("+password");
+  //if user is not found in DB
+  if (!user) {
+    return next(
+      new CustomError("Email or password does not match or exist", 400)
+    );
+  }
+
+  // match the password
+  const isPasswordCorrect = await user.isValidatedPassword(password);
+
+  // if password do not match
+  if (!isPasswordCorrect) {
+    return next(
+      new CustomError("Email or password does not match or exist", 400)
+    );
+  }
+
+  //if all goes good and we send the token
+  cookieToken(user, res);
+});
+
+exports.logout = BigPromise(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logout success",
+  });
+});
